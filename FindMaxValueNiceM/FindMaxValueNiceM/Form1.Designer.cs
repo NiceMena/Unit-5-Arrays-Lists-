b8c3ae7a98e865a7dd310c383797e716991ef281
Nice Mena <nice.mena@ocsbtudent.ca>
2018-12-04 10:52:41 -0500

    changes

--- FindMaxValueNiceM/FindMaxValueNiceM/Form1.Designer.cs
+++ FindMaxValueNiceM/FindMaxValueNiceM/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FindMaxValueNiceM
 {
-    partial class Form1
+    partial class frmFindMaxValue
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.lstMaxValue = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(146, 32);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.AutoSize = true;
+            this.lblMaxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaxValue.Location = new System.Drawing.Point(77, 367);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(57, 20);
+            this.lblMaxValue.TabIndex = 1;
+            this.lblMaxValue.Text = "label1";
+            // 
+            // lstMaxValue
+            // 
+            this.lstMaxValue.BackColor = System.Drawing.Color.MediumPurple;
+            this.lstMaxValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstMaxValue.FormattingEnabled = true;
+            this.lstMaxValue.ItemHeight = 20;
+            this.lstMaxValue.Location = new System.Drawing.Point(81, 85);
+            this.lstMaxValue.Name = "lstMaxValue";
+            this.lstMaxValue.Size = new System.Drawing.Size(224, 244);
+            this.lstMaxValue.TabIndex = 2;
+            // 
+            // frmFindMaxValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.MediumPurple;
+            this.ClientSize = new System.Drawing.Size(449, 462);
+            this.Controls.Add(this.lstMaxValue);
+            this.Controls.Add(this.lblMaxValue);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmFindMaxValue";
+            this.Text = "FindMaxValue";
+            this.Load += new System.EventHandler(this.frmFindMaxValue_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMaxValue;
+        private System.Windows.Forms.ListBox lstMaxValue;
     }
 }
 
--- FindMaxValueNiceM/FindMaxValueNiceM/Form1.cs
+++ FindMaxValueNiceM/FindMaxValueNiceM/Form1.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Nice Mena
+ * Created on: 30-11-18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #36 - Find Max Value
+ * This program finds the max value of numbers in an array
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +17,76 @@ using System.Windows.Forms;
 
 namespace FindMaxValueNiceM
 {
-    public partial class Form1 : Form
+    public partial class frmFindMaxValue : Form
     {
-        public Form1()
+        private int GetMaxValue(int[] tmpArrayOfInts)
+        {
+            int maxValue = -1;
+            int tmpMaxVal;
+
+            foreach (int value in tmpArrayOfInts)
+            {
+                tmpMaxVal = value;
+                if(tmpMaxVal > maxValue)
+                {
+                    maxValue = tmpMaxVal;
+                }
+            }
+            return maxValue;
+            
+        }
+        public frmFindMaxValue()
         {
             InitializeComponent();
+            // hiding the label
+            this.lblMaxValue.Hide();
+;        }
+    
+      
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // declaring local vaiables
+            const int MAX_ARRAY_SIZE = 10;
+
+            const int MAX_VAL = -1;
+
+           
+            // CREATING A RANDOM NUM GEN
+            Random randomNumGen = new Random();
+
+            // clearing the lst bbox
+            this.lstMaxValue.Items.Clear();
+
+            // creating the array
+            int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
+
+            int index, randomNumber, maxValue;
+
+
+            for (index = 0; index < MAX_ARRAY_SIZE; index++)
+            {
+                // generating a random num
+                randomNumber = randomNumGen.Next(1, 500 + 1);
+                arrayOfIntegers[index] = randomNumber;
+
+                // adding the numbers to the lst box
+                lstMaxValue.Items.Add(randomNumber);
+
+                // refreshing the lst box
+                this.Refresh();
+            }
+         
+            // get the max value in the array 
+            maxValue = GetMaxValue(arrayOfIntegers);
+
+            // display max value
+            this.lblMaxValue.Show();
+            this.lblMaxValue.Text = " The max Value is: " + maxValue;
+        }
+
+        private void frmFindMaxValue_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
